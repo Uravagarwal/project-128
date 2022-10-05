9b0686b370c9dd4851e8c023853786807d5b10b0
Uravagarwal <96483788+Uravagarwal@users.noreply.github.com>
2022-10-05 14:55:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 song="";
+song_2="";
 
 leftWrist_X = 0;
 leftWrist_Y = 0;
+score_leftWrist = 0;
 
 rightWrist_x = 0;
 rightWrist_Y = 0;
@@ -9,6 +11,7 @@ rightWrist_Y = 0;
 function preload()
 {
     song = loadSound("music.mp3");
+    song_2 = loadSound("music2.mp3")
 }
 
 function setup()
@@ -31,6 +34,17 @@ function modelLoaded()
 function draw()
 {
     image(video,0,0,600,500);
+
+    song.isPlaying()
+
+    fill("yellow");
+    stroke("red");
+
+    if(score_leftWrist>0.2)
+    {
+        circle(leftWrist_X, leftWrist_Y , 20);
+        song_2.stop()
+    }
 }
 
 function play()
@@ -41,10 +55,12 @@ function play()
 function gotPoses(results)
 {
     if(results.length>0)
-    {
+    {   console.log(results);
+        score_leftWrist = results[0].pose.keypoints[9].score;
         leftWrist_X = results[0].pose.leftWrist.x;
         leftWrist_Y = results[0].pose.leftWrist.y;
         console.log("leftWrist x = "+leftWrist_X+"leftWrist y = "+leftWrist_Y);
+        console.log("leftWrist_score"+score_leftWrist);
 
         rightWrist_X = results[0].pose.rightWrist.x;
         rightWrist_Y = results[0].pose.rightWrist.y;
